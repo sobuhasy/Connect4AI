f598ec9c64f8cf18874535616aa877dd6a7da84b
SobuHasy <79106639+sobuhasy@users.noreply.github.com>
2023-10-14 17:00:30 +0200

    Removed the yellow circle from before the AI's turn

--- connect4.py
+++ connect4.py
@@ -105,10 +105,8 @@ while not game_over:
 		if event.type == pygame.MOUSEMOTION:
 			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
 			posx = event.pos[0]
-			if turn == 0:
+			if turn == PLAYER:
 				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-			else: 
-				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
 		pygame.display.update()
 
 		if event.type == pygame.MOUSEBUTTONDOWN:
